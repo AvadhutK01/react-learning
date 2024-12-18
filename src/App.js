98bf8db8b2add2bd2cac0ba2bc9007471f275caa
AvadhutK01 <kelaskaravadhut052@gmail.com>
2024-12-18 14:22:33 +0530

    #1 use of usestate and even listener

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [count, setCount] = useState(0);
+
+  const countFunction = () => {
+    const interval = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 1000);
+    setTimeout(() => clearInterval(interval), 10000);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +25,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={countFunction}>{count}</button>
       </header>
     </div>
   );
